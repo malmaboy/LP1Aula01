44018ff335bb680feac719c4739a799098f64477
Hugo Carvalho <huguinho66@live.com>
2020-02-27 10:30:37 +0000

    Comentários1

--- Argumentos/Program.cs
+++ Argumentos/Program.cs
@@ -2,6 +2,12 @@
 
 namespace Argumentos {
     class Program {
+        /// <summary>
+        ///  it doe a for.
+        /// </summary>
+        /// <param name="args">
+        /// you cant do comments here for the args argument. 
+        /// </param>
         static void Main (string[] args) {
             for (int i = 0; i < args.Length; i++) {
                 Console.WriteLine (args[i]);
