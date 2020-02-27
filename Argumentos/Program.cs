8f104a232040711b478360a1a9b3146f47a1be42
Hugo Carvalho <huguinho66@live.com>
2020-02-27 10:28:28 +0000

    Comentários

--- Argumentos/Program.cs
+++ Argumentos/Program.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 
-namespace Argumentos
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            for  (int i = 0; i < args.Length; i++)
-            {
-            Console.WriteLine(args[i]);
+namespace Argumentos {
+    class Program {
+        static void Main (string[] args) {
+            for (int i = 0; i < args.Length; i++) {
+                Console.WriteLine (args[i]);
             }
         }
     }
 }
+
+/* it does a for */
+
