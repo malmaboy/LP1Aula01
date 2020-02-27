9b2dca7cda26d58925412dc3ebb589bfc5ea6512
Hugo Carvalho <huguinho66@live.com>
2020-02-27 11:43:10 +0000

    strings

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string normal = "Hello world";
+            string tentativa = "Um tab\t e um copyright\u03A9";
+            string meh = @"""esta string esta' entre aspas""";
+            string meeh = "\"Esta stings esta entre aspas\"";
+            string try1 = @"C:\Users\Ana\Documents";
+            string try2 = @"Nao é um codigo Unicode \u1234, nao e' nova linha";
+            Console.WriteLine(normal);
+            Console.WriteLine(tentativa);
+            Console.WriteLine(meh);
+            Console.WriteLine(meeh);
+            Console.WriteLine(try1);
+            Console.WriteLine(try2);
+        }
+    }
+}
