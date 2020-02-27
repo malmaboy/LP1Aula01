ef90a7000e9a43a43c99034cda93bba28c729003
Hugo Carvalho <huguinho66@live.com>
2020-02-27 11:01:53 +0000

    int

---
+++ Tipovarios/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+
+namespace Tipovarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 20;
+            int numero = 354;
+            
+            Console.WriteLine(number);
+            Console.WriteLine(numero);
+        }
+
+
+
+            
+    }
+}
