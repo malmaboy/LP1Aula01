42e7b49167297c6c9e6b6196c82ccef5b99f3eb5
Hugo Carvalho <huguinho66@live.com>
2020-02-27 11:21:46 +0000

    comments

--- Tipovarios/Program.cs
+++ Tipovarios/Program.cs
@@ -8,19 +8,28 @@ namespace Tipovarios
     {
         static void Main(string[] args)
         {
+            /// <summary>
+            /// inteiros
+            /// </summary>
             int number = 20;
             int numero = 354;
-
+            /// <summary>
+            /// reais
+            /// </summary>
             double yolo = 0.78;
             double meh = 0.80;
-
+            /// <summary>
+            /// characteres unicode
+            /// </summary>
             char meeh = '\u0156';
             char toto = '\u0186';
-
+            /// <summary>
+            /// booleanos
+            /// </summary>
             bool y = true;
             bool e = false;
 
-
+            //prints
             Console.WriteLine(number);
             Console.WriteLine(numero);
             Console.WriteLine(yolo);
