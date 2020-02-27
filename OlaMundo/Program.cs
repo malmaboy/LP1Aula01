f940d956d35dd0610b46f7f34a6768794ebb16b0
Hugo Carvalho <huguinho66@live.com>
2020-02-27 09:47:18 +0000

    Add bye World

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -7,6 +7,7 @@ namespace OlaMundo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bye World");
         }
     }
 }
